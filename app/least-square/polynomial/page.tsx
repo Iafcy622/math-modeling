1f5c45dd208271e122d21441c7e38e6a1d9618fc
Iafcy622 <frankiecheung512@gmail.com>
2024-03-31 00:38:00 +0800

    Added page for Least-square polynomial fitting

---
+++ app/least-square/polynomial/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ScatterController,
+  LineController,
+  ScatterDataPoint,
+  ChartOptions,
+  ChartData,
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+import { generateData, polyfit } from "@/lib/generate";
+import KaTeX from "@/components/katex-expression";
+import { useState } from "react";
+import { Slider } from "@/components/ui/slider";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ScatterController,
+  LineController,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function Page() {
+  const alphabet = ["a", "b", "c", "d", "e", "f"];
+
+  const pts = [
+    { x: 1, y: 8 },
+    { x: 2, y: 12 },
+    { x: 3, y: 9 },
+    { x: 4, y: 3 },
+    { x: 5, y: 7 },
+    { x: 6, y: 2 },
+  ];
+
+  const [order, setOrder] = useState(1);
+
+  const { param, expression, evalExpression } = polyfit(pts, order);
+
+  const [xValues, yValues] = generateData(evalExpression, 0, 7, 0.5);
+
+  const options: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    aspectRatio: 1.25,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Least-squares criterion",
+      },
+    },
+    scales: {
+      y: {
+        suggestedMax: 8,
+        suggestedMin: 0,
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const data: ChartData = {
+    labels: xValues,
+    datasets: [
+      {
+        type: "line" as const,
+        label: "y=f(x)",
+        data: yValues,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        type: "scatter" as const,
+        label: "data",
+        data: pts,
+        backgroundColor: "rgb(9, 249, 242)",
+      },
+    ],
+  };
+
+  function handleInputChange(val: number) {
+    setOrder(val);
+  }
+
+  return (
+    <>
+      <h1 className="text-2xl my-4">Least-squares Criterion (Polynomial)</h1>
+      <Separator />
+
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <div className="py-4">
+          <form className="mb-8">
+            <Label htmlFor="order">Order : {order}</Label>
+            <Slider
+              className="mb-4"
+              defaultValue={[order]}
+              max={5}
+              min={1}
+              step={1}
+              id="order"
+              onValueChange={([val]) => handleInputChange(val)}
+            />
+          </form>
+
+          <span className="text-xl">Model function: </span>
+          <KaTeX
+            texExpression={`f(x)=${expression}`}
+            className="mt-2 text-lg"
+          />
+        </div>
+        <div>
+          <Chart type="line" data={data} options={options} height={400} />
+        </div>
+      </div>
+      <div className="mt-12">
+        <h2 className="text-2xl my-4">Derive the model</h2>
+        <Separator />
+        <KaTeX
+          texExpression={`\\text{Assume the model function is }f(x;${alphabet
+            .slice(0, order + 1)
+            .join(",")})=${alphabet
+            .slice(0, order + 1)
+            .map(
+              (char, i) =>
+                char +
+                (order - i > 1 ? `x^${order - i}` : order - i == 1 ? "x" : "")
+            )
+            .join("+")}`}
+          className="mt-6 text-lg"
+        />
+        <KaTeX
+          texExpression="\displaystyle\text{We would like to minimize }S(a,b) = \sum_{i=1}^m (y_i-f(x_i))^2"
+          className="mt-2 text-lg"
+        />
+        <KaTeX texExpression="\displaystyle \cfrac{\partial S}{\partial a} = \sum_{i=1}^m (-2x_i(y_i-ax_i-b)) = 0" />
+        <KaTeX texExpression="\displaystyle \cfrac{\partial S}{\partial b} = \sum_{i=1}^m (-2(y_i-ax_i-b)) = 0" />
+        <KaTeX
+          texExpression={`\\therefore ${param.map(
+            (p, i) => `${alphabet[i]}=${p.toFixed(2)}`
+          )}`}
+          className="mt-2 text-lg"
+        />
+        <KaTeX
+          texExpression={`\\text{Our model function is }f(x)=${expression}`}
+          className="mt-2 text-lg"
+        />
+      </div>
+    </>
+  );
+}
--- lib/generate.ts
+++ lib/generate.ts
@@ -43,3 +43,66 @@ export function polyfit1d(data: Point[]): { a: number; b: number } {
 
   return { a: res.get([0]), b: res.get([1]) };
 }
+
+export function polyfit(
+  data: Point[],
+  order: number
+): { param: number[]; expression: string; evalExpression: string } {
+  const n = data.length;
+  const sumX = [];
+
+  for (let i = order * 2; i > 0; i--) {
+    let temp = 0;
+    for (let j = 0; j < n; j++) {
+      temp += Math.pow(data[j].x, i);
+    }
+    sumX.push(temp);
+  }
+  sumX.push(n);
+
+  const arrA = [];
+  for (let i = 0; i < order + 1; i++) {
+    arrA.push(sumX.slice(i, i + order + 1));
+  }
+
+  const arrB = [];
+  for (let i = order; i >= 0; i--) {
+    let temp = 0;
+    for (let j = 0; j < n; j++) {
+      temp += Math.pow(data[j].x, i) * data[j].y;
+    }
+    arrB.push(temp);
+  }
+
+  const A = math.matrix(arrA);
+  const B = math.matrix(arrB);
+  const res = math.multiply(math.inv(A), B);
+
+  const param = [];
+  let expression = "";
+  let evalExpression = "";
+  for (let i = 0; i < order + 1; i++) {
+    param.push(res.get([i]));
+
+    if (res.get([i]) >= 0 && i != 0) {
+      expression += "+";
+      evalExpression += "+";
+    }
+    if (i == order) {
+      expression += `${res.get([i]).toFixed(2)}`;
+      evalExpression += `${res.get([i]).toFixed(2)}`;
+    } else if (i == order - 1) {
+      expression += `${res.get([i]).toFixed(2)}x`;
+      evalExpression += `${res.get([i]).toFixed(2)} * x`;
+    } else {
+      expression += `${res.get([i]).toFixed(2)}x^${order - i}`;
+      evalExpression += `${res.get([i]).toFixed(2)} * x**${order - i}`;
+    }
+  }
+
+  return {
+    param,
+    expression,
+    evalExpression,
+  };
+}
